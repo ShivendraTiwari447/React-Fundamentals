5f36981c6c43f69e78d1124aebf0c23650b1942e
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-09-08 21:18:44 +0530

    creating customReact

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,9 +1,33 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+const anotherElement = (
+    <a
+        href="https://google.com"
+        target="_blank"
+    >
+        Visit Google
+    </a>
+)
+
+
+const anotherUser = "shivendra Tiwari"
+
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    'click me to visit google',
+    anotherElement
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    reactElement
 )
